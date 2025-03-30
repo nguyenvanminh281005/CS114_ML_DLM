13e4ecf2a89d4dbffa7fc89a8ef26d1c82a21e20
nguyenvanminh281005 <minhphantom.123@gmail.com>
2025-03-30 12:45:33 +0700

    update files - 2025-03-30 12:45:32

---
+++ backend/git-auto-push.ps1
@@ -0,0 +1,9 @@
+# Tự động add tất cả thay đổi
+git add .
+
+# Commit với thông điệp tự động
+$commitMessage = "update files - $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')"
+git commit -m $commitMessage
+
+# Push lên remote repository
+git push -u origin master  # Hoặc thay bằng nhánh bạn đang sử dụng
